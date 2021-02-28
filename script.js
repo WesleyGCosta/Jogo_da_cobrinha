e16b791c75b6aae1acb33de675ac7a14ef25b8aa
WesleyGCosta <wesleygomescosta825@gmail.com>
2021-02-28 12:49:26 -0400

    Aplicando css

--- script.js
+++ script.js
@@ -47,10 +47,12 @@ function iniciarJogo() {
 
     for(i = 1; i < snake.length; i++)
     {
+        let contador = 0;
+        contador++;
         if(snake[0].x == snake[i].x && snake[0].y == snake[i].y)
         {
             clearInterval(jogo);
-            alert('Game Over :(');
+            alert('Game Over :(' + contador + " pontos");
         }
     }
 
