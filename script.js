ee1bd4978c416a525ecb0e893959a8511f34d752
WesleyGCosta <wesleygomescosta825@gmail.com>
2021-02-28 10:07:04 -0400

    definir aleatoridade da fruta e tamanho da cobra

--- script.js
+++ script.js
@@ -57,7 +57,14 @@ function iniciarJogo() {
     if(direction == "up") sanakY += box;
     if(direction == "down") sanakY += box;
 
-    snake.pop();
+    if(snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    }
+    else{
+        food.x = Math.floor(Math.random() * 15 + 1) * box;
+        food.y = Math.floor(Math.random() * 15 + 1) * box;
+    }
+
 
     let newHead = {
         x: snakeX,
